84762dfc17a14e07d20cba85aae9f50f0c940e3a
huangkun <huangkun@126com>
2021-02-01 10:02:58 +0800

    first commit

--- go.mod
+++ go.mod
@@ -2,4 +2,15 @@ module component
 
 go 1.15
 
-require github.com/sirupsen/logrus v1.7.0
+require (
+	github.com/dearkk/component v0.0.0-20210129033259-7bebaf275701
+	github.com/prometheus-operator/prometheus-operator/pkg/client v0.45.0
+	github.com/sirupsen/logrus v1.4.2
+	istio.io/client-go v1.8.2
+	k8s.io/client-go v0.19.2
+)
+
+replace (
+	github.com/dearkk/component v0.0.0-20210129033259-7bebaf275701 => ../component
+	github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring v0.45.0 => ./prometheus-operator-0.42.1/pkg/apis/monitoring
+)
---
+++ kube/context.go
@@ -0,0 +1,52 @@
+package kube
+
+import (
+	"github.com/dearkk/component/klog"
+	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/client/versioned/typed/monitoring/v1"
+	versionedclient "istio.io/client-go/pkg/clientset/versioned"
+	client "k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+type Context struct {
+	kube       *client.Clientset
+	ist        *versionedclient.Clientset
+	prometheus *monitoringv1.MonitoringV1Client
+}
+
+func NewContext(kubeconfigPath string) *Context {
+	var err error
+	var config *rest.Config
+	var context Context
+	config, err = clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+	if err != nil {
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			klog.Panicf(err.Error())
+		}
+		klog.Infof("init from cluster config\n")
+	} else {
+		klog.Infof("init from file config: %s\n", kubeconfigPath)
+	}
+	context.kube, err = client.NewForConfig(config)
+	if err != nil {
+		klog.Errorf(err.Error())
+	} else {
+		klog.Infof("init k8s ok.")
+	}
+
+	context.ist, err = versionedclient.NewForConfig(config)
+	if err != nil {
+		klog.Errorf("Failed to create istio client: %s", err)
+	} else {
+		klog.Infof("init istio ok.")
+	}
+	context.prometheus, err = monitoringv1.NewForConfig(config)
+	if err != nil {
+		klog.Errorf(err.Error())
+	} else {
+		klog.Infof("init prometheus ok.")
+	}
+	return &context
+}
